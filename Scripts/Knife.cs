6714809c7289869888ce79b3d655c7e685fc09fc
Роман <41577753+nerfmee@users.noreply.github.com>
2020-04-28 12:57:34 +0300

    Add files via upload

---
+++ Scripts/Knife.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knife : MonoBehaviour
+{
+
+    [SerializeField]
+    private Vector2 throwForce;
+
+    private bool isActive = true;
+
+    private Rigidbody2D rb;
+    private BoxCollider2D knifeCollder;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        knifeCollder = GetComponent<BoxCollider2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && isActive)
+        {
+            rb.AddForce(throwForce, ForceMode2D.Impulse);
+            rb.gravityScale = 1;
+            GameController.Instance.GameUI.DecrementDisplayedKnifeCount();
+        }
+    }
+
+    private void OnCollisionEnter2D (Collision2D collision)
+    {
+        if (!isActive)
+            return;
+
+        isActive = false;
+
+        if (collision.collider.tag == "Log")
+        {
+            GetComponent<ParticleSystem>().Play();
+            rb.velocity = new Vector2(0, 0);
+            rb.bodyType = RigidbodyType2D.Kinematic;
+            this.transform.SetParent(collision.collider.transform);
+
+            knifeCollder.offset = new Vector2(knifeCollder.offset.x, -0.4f);
+            knifeCollder.size = new Vector2(knifeCollder.size.x, 1.2f);
+
+            GameController.Instance.OnSuccessfulKnifeHit();
+        }
+        else if(collision.collider.tag == "Knife")
+        {
+            rb.velocity = new Vector2(rb.velocity.x, -2);
+            GameController.Instance.StartGameOverSequence(false);
+        }
+    }
+}
